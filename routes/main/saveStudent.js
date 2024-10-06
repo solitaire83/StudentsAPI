7ddd018a171d3fde719a7694fa347478caba8666
solitaire <fmailiulian@gmail.com>
2024-10-06 11:07:26 +0300

    salvarea studentului necesita respectarea unui format standard.

--- routes/main/saveStudent.js
+++ routes/main/saveStudent.js
@@ -3,9 +3,17 @@ const router = express.Router();
 const readStudents = require("../../funcs/readStudents");
 const writeStudents = require("../../funcs/writeStudents");
 
+const {StudentFormat, StudentExistent} = require("../../funcs/formatStudents");
+
 router.post("/saveStudent", (req, res) => {
   const students = readStudents();
   const studentnou = req.body;
+
+  const format = StudentFormat(studentnou);
+  if(!format.corect) return res.status(400).json({err: format.err});
+  const student = StudentExistent(students, studentnou);
+  if(student.existent) return res.status(400).json({err: student.err});
+
   students.push(studentnou);
   writeStudents(students);
   res.status(201).json(studentnou);
