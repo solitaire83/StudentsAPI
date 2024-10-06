5859349420c16ed9478d305a30f4e151b31a7cfb
solitaire <fmailiulian@gmail.com>
2024-10-06 11:24:45 +0300

    update v1.2.0

--- routes/api.js
+++ routes/api.js
@@ -4,15 +4,22 @@ const router = express.Router();
 const load = require("./main/loadStudents");
 const save = require("./main/saveStudent");
 const remove = require("./main/rmvStudent");
+const edit = require("./main/editStudent");
+const count = require("./main/countStudents");
 
 const sortnume = require("./sorting/sortnume");
 const sortnota = require("./sorting/sortnota");
 
+const studgen = require("./others/studgen");
+
 router.use(load);
 router.use(save);
 router.use(remove);
+router.use(edit);
+router.use(count);
 router.use(sortnume);
 router.use(sortnota);
+router.use(studgen);
 
 router.get("/", (req, res) => {
     res.send(`
@@ -21,7 +28,9 @@ router.get("/", (req, res) => {
    <h1 style="margin: 0;">Bn te-am gasit, astea sunt toate rutele:</h1>
    <div style="font-size: 30px;">
    GET -> <a href="/api/loadStudents" style="color: green;">/api/loadStudents</a><br>
+   GET -> <a href="/api/countStudents" style="color: green;">/api/countStudents</a><br>
    POST -> <a href="/api/saveStudent" style="color: green;">/api/saveStudents</a><br>
+   PUT -> <a href="/api/editStudent" style="color: green;">/api/editStudent</a><br>
    DELETE -> <a href="/api/rmvStudent" style="color: green;">/api/rmvStudents</a><br>
    <br>
    GET -> <a href="/api/sorting/sortnume" style="color: green;">/api/sorting/sortnume</a><br>
